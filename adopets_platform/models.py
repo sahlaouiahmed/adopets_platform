ee2cf6c68ea5b4bb111f4f9b8e8c6514f29c8ebf
Ahmed Sahlaoui <sahlaouiahmad@outlook.com>
2024-10-09 21:21:56 +0000

    creating the pet model

--- adopets_platform/models.py
+++ adopets_platform/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    breed = models.CharField(max_length=100)
+    age = models.IntegerField()
+    description = models.TextField()
+    photo = models.ImageField(upload_to='pet_photos/')
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posted_pets')
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
