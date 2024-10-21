c24bfd749658a48a9ea07dbc3b35872d743a6b8d
Ahmed Sahlaoui <sahlaouiahmad@outlook.com>
2024-10-21 10:06:08 +0000

    adding secret key to env file

--- CI_project4/settings.py
+++ CI_project4/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
 import sys
 import dj_database_url
 if os.path.isfile('env.py'):
